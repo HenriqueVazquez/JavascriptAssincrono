fc301df90514594265c8b1c1cb00a8cc41674ba9
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-03-14 18:21:34 -0300

    Promises com Fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-fetch("https://api.github.com/users/henriquevazquez").then((response) => {
-  response.json().then((data) => {
-    fetch(data.repos_url).then((res) => res.json().then((d) => console.log(d)));
-  });
-});
+fetch("https://api.github.com/users/henriquevazquez")
+  .then((response) => response.json())
+  .then((data) => fetch(data.repos_url))
+  .then((res) => res.json()
+    .then((d) => console.log(d)));
